60ecd2031b85858871af2e61b698ef19c7bd4954
rulerjupiter <59585832+rulerjupiter@users.noreply.github.com>
2020-03-25 16:51:57 -0400

    Add files via upload

---
+++ DestroyByBoundary.cs
@@ -0,0 +1,18 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public AudioSource musicSource;
+
+    public AudioClip musicClipOne;
+
+    private Rigidbody rb;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+
+    private float nextFire;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        if  (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            // GameObject clon e=
+            Instantiate(shot,shotSpawn.position, shotSpawn.rotation);
+            
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            musicSource.clip = musicClipOne;
+            musicSource.Play();
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            musicSource.Stop();
+        }
+        
+    } 
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+             Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+             0.0f,
+             Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
